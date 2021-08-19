1927526e2f18dba6204ca645ed8187abdefc3be5
Sajjad Ahmad <jogimar14@gmail.com>
2021-08-19 14:02:08 +0500

    Add navigation bar

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+const Nav = () => {
+  return (
+    <div>
+      <nav className="navigation">
+        <a href="#">Get the app</a>
+        <div className="nav-items">
+          <div className="nav-item">Add Restaurant</div>
+          <div className="nav-item">Log in</div>
+          <div className="nav-item">Sign Up</div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav;
