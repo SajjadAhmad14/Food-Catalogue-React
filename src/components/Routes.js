5eaeeb86fb2426056aaa3c20cb23e9009815c7e3
Sajjad Ahmad <jogimar14@gmail.com>
2021-09-01 19:32:35 +0500

    Add sign up page and it's routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -8,6 +8,7 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMobile } from '@fortawesome/free-solid-svg-icons';
 import SignIn from './SignIn';
+import SignUp from './SignUp';
 import App from '../App';
 
 const Routes = () => (
@@ -18,7 +19,7 @@ const Routes = () => (
         <div className="nav-items">
           <div className="nav-item">Add Restaurant</div>
           <Link to="/signin"><div className="nav-item">Log in</div></Link>
-          <div className="nav-item">Sign Up</div>
+          <Link to="/signup"><div className="nav-item">Sign Up</div></Link>
         </div>
       </nav>
     </div>
@@ -26,6 +27,7 @@ const Routes = () => (
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/signup" component={SignUp} />
     </Switch>
   </Router>
 );
